2350cf63d9b5d473a4aa6740df8af6f9f3806a92
Larry Botha <lawrenceguy@gmail.com>
2025-01-11 16:13:52 +0530

    test(lexer): test for enums

---
+++ src/lexer/lexer_test.go
@@ -0,0 +1,48 @@
+package lexer
+
+import (
+	"testing"
+
+	"monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := "=+(){},;"
+
+	tests := []struct {
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		token := l.NextToken()
+
+		if token.Type != tt.expectedType {
+			t.Fatalf(
+				"tests[%d] - token type incorrect. expected=%q, got=%q", i,
+				tt.expectedType,
+				token.Type,
+			)
+		}
+
+		if token.Literal != tt.expectedLiteral {
+			t.Fatalf(
+				"tests[%d] - token literal incorrect. expect=%q, got=%q", i,
+				tt.expectedLiteral,
+				token.Literal,
+			)
+		}
+	}
+}
